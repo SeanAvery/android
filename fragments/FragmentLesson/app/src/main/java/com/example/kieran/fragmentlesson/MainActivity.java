f9e4ad12c59f800b1059472b91425cb27849e8e6
Sean Pollock <seanpollock3344@gmail.com>
2016-03-21 13:48:50 -0500

    adding android projects

--- fragments/FragmentLesson/app/src/main/java/com/example/kieran/fragmentlesson/MainActivity.java
+++ fragments/FragmentLesson/app/src/main/java/com/example/kieran/fragmentlesson/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.kieran.fragmentlesson;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -48,11 +53,41 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             //TODO Do Fun things with Fragments
-            Toast.makeText(getApplicationContext(), planets.get(position), Toast.LENGTH_LONG).show();
+            selectPlanet(position);
+            // Toast.makeText(getApplicationContext(), planets.get(position), Toast.LENGTH_LONG).show();
             drawerLayout.closeDrawer(listView);
         }
     }
 
+    private void selectPlanet(int position) {
+        PlanetFragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager manager = getFragmentManager();
+        manager.beginTransaction().replace(R.id.frame_layout, fragment).commit();
+    }
+
+    public static class PlanetFragment extends Fragment{
+        private static final String PLANET_NUMBER = "planet";
+
+        public PlanetFragment () {
+            // Intentionally left empty
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_planet, container, false);
+            int planetNumber = getArguments().getInt(PLANET_NUMBER);
+            String planetName = getResources().getStringArray(R.array.planets_array)[planetNumber];
+            TextView textView = (TextView) view.findViewById(R.id.plant_text_view);
+            textView.setText(planetName);
+            return view;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
